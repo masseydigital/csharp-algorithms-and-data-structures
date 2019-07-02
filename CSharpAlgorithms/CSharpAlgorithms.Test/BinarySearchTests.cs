d9f5889baec7a82ee6d2cf47023e3bbc2007b7ff
masseydigital <acemgaming@gmail.com>
2019-07-02 19:20:15 -0400

    Updated Readme for Symbol Tables and added code for Binary Search Tree and Test.

---
+++ CSharpAlgorithms/CSharpAlgorithms.Test/BinarySearchTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CSharpAlgorithms.Test
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        [Test]
+        public void BinarySearch_SortedInput_ReturnsCorrectIndex()
+        {
+            int[] input = { 0, 3, 4, 5, 7, 9, 12, 15, 22 };
+
+            Assert.AreEqual(2, Searching.BinarySearch(input, 4));
+            Assert.AreEqual(4, Searching.BinarySearch(input, 8));
+            Assert.AreEqual(6, Searching.BinarySearch(input, 15));
+            Assert.AreEqual(7, Searching.BinarySearch(input, 22));
+
+            Assert.AreEqual(2, Searching.BinarySearch(input, 4));
+            Assert.AreEqual(4, Searching.BinarySearch(input, 8));
+            Assert.AreEqual(6, Searching.BinarySearch(input, 15));
+            Assert.AreEqual(7, Searching.BinarySearch(input, 22));
+        }
+    }
+}
---
+++ CSharpAlgorithms/CSharpAlgorithms/Searching.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgorithms
+{
+    public class Searching
+    {
+        public static int RecursiveBinarySearch(int[] array, int value)
+        {
+            return InternalRecursiveBinarySearch(0, array.Length);
+            int InternalRecursiveBinarySearch(int low, int high)
+            {
+                if(low >= high)
+                {
+                    return -1;
+                }
+
+                int mid = (low + high) / 2;
+
+                if(array[mid] == value)
+                {
+                    return mid;
+                }
+
+                if(array[mid] < value)
+                {
+                    return InternalRecursiveBinarySearch(mid + 1, high);
+                }
+
+                return InternalRecursiveBinarySearch(low, mid);
+            }
+        }
+
+        public static int BinarySearch(int[] array , int value)
+        {
+            int low = 0;
+            int high = array.Length;
+
+            while(low < high)
+            {
+                int mid = (low + high) / 2;
+
+                if(array[mid] == value)
+                {
+                    return mid;
+                }
+
+                if(array[mid] < value)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
